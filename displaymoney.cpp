6a6a74941d576ed4e220cc85da61ea263deaa3a3
anieves001 <40726199+anieves001@users.noreply.github.com>
2019-11-01 19:22:18 -0400

    Update displaymoney.cpp

--- displaymoney.cpp
+++ displaymoney.cpp
@@ -3,7 +3,6 @@
 #include "hands.h"
 
 using  namespace std;
-using namespace nieves_hands;
 
 string displaymoney(double money);
 
@@ -19,6 +18,20 @@ int main(){
 
 string displaymoney(double money){
 	
+	/*	
+			Each if statement addresses the placement of commas in 1,000 10,000 and 100,000 respectively.
+			Larger values are simply extensions with the same comma placements as the former.
+			Since a double that is converted to a string produces a decimal with six 0's appended to it,
+			the modulus of the number size with values 11, 12 and 13 produce values that are divisible by 3 for
+			1,000 10,000 and 100,000 respectively.
+			11,14,17  		12,15,18  		13,16,19		  
+			1000.000000      	10000.000000   		100000.000000   	 	
+			1000000.000000		10000000.000000		100000000.000000
+			1000000000.000000	10000000000.000000	100000000000.000000
+			We also make sure we dont place any commas after the decimal.
+
+
+		*/	
 	string dollars=to_string(money);
 	string accum;
 	char test;
@@ -27,17 +40,7 @@ string displaymoney(double money){
 	for(int i =0;i<dollars.size()-4;i++){
 		accum+=dollars[i];
 		test=dollars[i+1];
-		/*	11,14,17  12,15,18  13,16,19  14,17,20  15,18,21
-			1000      10000     100000    1000      10000
-			Each if statement addresses the placement of commas in 1,000 10,000 and 100,000 respectively.
-			Larger values are simply extensions with the same comma placements as the former.
-			Since a double that is converted to a string produces a decimal with six 0's appended to it,
-			the modulus of the number size with values 11, 12 and 13 produce values that are divisible by 3 for
-			1,000 10,000 and 100,000 respectively.
-			We also make sure we dont place any commas after the decimal.
-
-
-		*/	
+		
 		if(((dollars.size()%11)%3==0)&&!(test=='.')&&(i<dollars.size()-7)){
 			//condition value is in the 1,000's 1,000,000's 1,000,000,000's...etc.	
 			if(i==0||j==3){	
